9d430bc16104f2df852809bc1bd23d211fe3e4c4
Kevin Spence <KevinSpence95@gmail.com>
2023-02-23 19:50:46 -0500

    set up App component with basic states to track the user input and the current location being displayed. location can be changed on button click

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
 import useGetWeather from './useGetWeather';
+import { useState } from 'react';
 
 function App() {
-  
-  
-  let weatherData = useGetWeather('Houston','US')
+  let [inputs, setInputs] = useState([])
+  let [location, setLocation] = useState(["Columbus",'Ohio'])
 
+  function getInputs(e) {
+      let cleanedInput = [e.target.value.toUpperCase().split(',')[0], e.target.value.toUpperCase().split(',')[1]]
+      setInputs(cleanedInput)
+  }
+
+  function changeLocation(e) {
+    e.preventDefault()
+    setLocation(inputs)
+  }
+
+  let weatherData = useGetWeather(...location)
 
   return (
     <div className="WeatherApp">
-     {weatherData.tempF}
+    <input type="text" placeholder='City, State/CC' onChange={getInputs}/>
+    <button type='submit' onClick={changeLocation}>Change Location</button>
+    {JSON.stringify(inputs)}
     </div>
   );
 }
